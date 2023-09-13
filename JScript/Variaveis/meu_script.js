33f7dc43368f36d69399307f47f76d8b1d469670
Yuri Rocha <maskedkiller@outlook.com>
2023-09-12 21:04:33 -0300

    Js

---
+++ JScript/Variaveis/meu_script.js
@@ -0,0 +1,15 @@
+//String
+var texto = 'Curso de JavaScript'
+
+//Number
+var numeroInteiro = -7
+var numeroFracionado = 10.75
+
+//boolean
+var teste = true
+
+//alert (numeroFracionado) //Abrir um dialog - Bom pra Debug
+
+// document.write(texto)
+
+console.log(numeroInteiro)
